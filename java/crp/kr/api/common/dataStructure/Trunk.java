c8927a18f6558e788841878f4104dc63aa958064
ChoHyunGook <whgusrnr7755@gmail.com>
2022-05-12 21:52:42 +0900

    ArrayList,Hashmap

--- java/crp/kr/api/common/dataStructure/Trunk.java
+++ java/crp/kr/api/common/dataStructure/Trunk.java
@@ -19,31 +19,46 @@ import java.util.stream.Collectors;
  * 2022-05-12 chohyungook 최초 생성
  */
 @Data
-public class Trunk<T>{
-    private final Map<String,Trunk> map;
-    private final List<Trunk> list;
+public class Trunk<T,U>{
+    private Map<T,U> map;
     public Trunk() {
-        map = new HashMap<>();
-        this.list = new ArrayList<>();
+        this.map = new HashMap<>();
     }
     // save,update,delete,findAll,findByName,findById,count,existsById,clear
     //add,set,remove,get(),X,get(i),size,X,clear
 
-    public void save(Trunk trunk){map.put(trunk.get(),trunk);}
-    public void update(Trunk trunk){map.replace(trunk.get(),trunk);}
-    public void delete(Trunk trunk){map.remove(trunk.get());}
-    public List<T> findById(String id) {return map.get(id);}
-    public T findByName(String name) {
-        List<T> t =new ArrayList<T>();
-        map.forEach((key,value)->{
-            if(value..equals(name)){
-              t.add(value);
-            }
-        });
-        return t;}
-    public T findByAll(String id) {return map.values().stream().collect(Collectors.toList());}
-    public int count(){return map.size();}
-    public void clear(){map.clear();}
-    public boolean existsById(String id){return map.containsKey(id);}
+    public void put(T t,U u) {
+        map.put(t, u);
+    }
+
+    public void replace(T t,U u) {
+        map.replace(t,u);
+    }
+
+    public void remove(Object t) {
+        map.remove(t);
+    }
+
+    public U findById(String id) {
+        return map.get(id);
+    }
+
+    public List<U> findByName(String name) {
+        List<U> list = new ArrayList<>();
+        return list;
+    }
+
+    public List<U> findAll() {
+        return map.values().stream().collect(Collectors.toList());
+    }
+
+    public int count() {
+        return map.size();
+    }
+
+    public boolean existsById(String id) {
+        return map.containsKey(id);
+    }
+
 
 }
